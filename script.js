0609af2500e4952dd69b6bf3427824a2ba9fdf49
gangasemwal82 <gangasemwal2025@gmail.com>
2025-04-21 21:18:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-// Simple Click Game
+document.getElementById("game").innerHTML = `
+    <h1 style="color:red">Battle Rush</h1>
+    <div id="target" style="
+        width:50px; height:50px; 
+        background:blue; 
+        cursor:pointer;
+    "></div>
+    <p>Score: <span id="score">0</span></p>
+`;
+
+let score = 0;
+document.getElementById("target").onclick = function() {
+    score++;
+    document.getElementById("score").textContent = score;
+    this.style.left = Math.random() * 300 + "px";
+    this.style.top = Math.random() * 300 + "px";
+};// Simple Click Game
 const game = document.getElementById("game");
 let score = 0;
 
